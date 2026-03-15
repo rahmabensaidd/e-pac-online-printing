442a23e25f666ca4afe82e49299e2be3c311e914
rahmabensaidd <bensaidrahma2024@gmail.com>
2026-03-15 15:11:53 +0100

    Initial commit : Structure du projet Spring Boot + Angular

---
+++ eprinting/src/main/java/tn/epac/eprinting/EprintingApplication.java
@@ -0,0 +1,13 @@
+package tn.epac.eprinting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EprintingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EprintingApplication.class, args);
+	}
+
+}
---
+++ eprinting/src/test/java/tn/epac/eprinting/EprintingApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.epac.eprinting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprintingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
