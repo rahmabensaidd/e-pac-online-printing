f419c4f95947bbd27a69ce0d540b026647999860
rahmabensaidd <bensaidrahma2024@gmail.com>
2026-04-28 11:08:25 +0100

    deleting unused directories + dockerfile for backend

--- mysql-init/init.sql
+++
@@ -1,4 +0,0 @@
--- Allow the application user from any host (Docker network)
-CREATE USER IF NOT EXISTS 'eprinting_user'@'%' IDENTIFIED BY 'eprinting_pass123';
-GRANT ALL PRIVILEGES ON eprinting_db.* TO 'eprinting_user'@'%';
-FLUSH PRIVILEGES;
